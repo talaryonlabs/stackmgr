c2a1675530dde46e4579747a413ceac8cdc40f80
Nico Ahsbahs <nico.ahsbahs@smartpoint.at>
2026-02-25 12:52:21 +0100

    Add initial setup for `Namespace` class in `StackManager.Proxy`
    - Introduce `Namespace` API controller with versioning and routing support.
    - Update `Program.cs` with necessary using directives for hosting and API functionality.

---
+++ src/StackManager.Proxy/Namespace.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talaryon.Toolbox.API.Client;
+using Talaryon.Toolbox.API.Hosting;
+
+namespace StackManager.Proxy;
+
+[ApiVersion("1.0")]
+[ApiRoute("namespace")]
+public class Namespace
+{
+    public void Get()
+    {
+        
+    }
+}
--- src/StackManager.Proxy/Program.cs
+++ src/StackManager.Proxy/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net.Mime;
+using Microsoft.AspNetCore.Builder.Internal;
+using Microsoft.Extensions.Hosting;
+
+using Talaryon.Toolbox.API.Hosting;
+
+
+
 Console.WriteLine("Hello, World!");
